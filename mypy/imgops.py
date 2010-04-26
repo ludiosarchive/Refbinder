33b3e4ec82aeabf60e2b63e18ea75510f8f71d1d
ivank <ivank@ludios.net>
2010-04-25 18:33:11 -0700

    mypy/imgops.py and test_imgops.py: cleanups and API changes

--- mypy/imgops.py
+++ mypy/imgops.py
@@ -7,31 +7,42 @@ import struct
 
 
 class BadImage(Exception):
-	"""Couldn't parse the bytes."""
+	"""
+	Couldn't parse the bytes.
+	"""
 
 
-def readin(stream, length, offset=0):
-	if offset != 0:
-		stream.seek(offset, 0)
+
+def _readIn(stream, length, offset=0):
+	stream.seek(offset, 0)
 	return stream.read(length)
 
 
-def png_size(stream):
+def pngSize(stream):
 	"""Return (width, height) of a PNG file object. Also works on MNG."""
 	width, height = -1, -1
-	if readin(stream, 4, 12) in ('IHDR', 'MHDR'):
+	if _readIn(stream, 4, 12) in ('IHDR', 'MHDR'):
 		width, height = struct.unpack("!II", stream.read(8))
 	return width, height
 
 
-def gif_size(stream):
+def gifSize(stream):
 	"""
 	From http://mail.python.org/pipermail/python-list/2007-June/617126.html
 	"""
-
 	# Skip over the identifying string, since we already know this is a GIF
-	buf = readin(stream, 5, 6)
+	buf = _readIn(stream, 5, 6)
 	if len(buf) != 5:
 		raise BadImage("Invalid/Corrupted GIF (bad header)")
-	(sw, sh, x) = struct.unpack("<HHB", buf)
+	sw, sh, x = struct.unpack("<HHB", buf)
 	return (sw, sh)
+
+
+def isPng(s):
+	"""
+	Returns C{True} if string C{s} starts with a PNG header.
+	"""
+	if not isinstance(s, str):
+		raise TypeError("isPng takes a str, not a %r" % (type(s),))
+
+	return s.startswith('\x89\x50\x4E\x47\x0D\x0A\x1A\x0A')
--- mypy/test/test_imgops.py
+++ mypy/test/test_imgops.py
@@ -4,27 +4,48 @@ from twisted.python.filepath import FilePath
 from mypy import imgops
 
 
-class ImageDimensions(unittest.TestCase):
+class ImageSizeTests(unittest.TestCase):
+	"""
+	Tests for L{imgops.pngSize} and L{imgops.gifSize}
+	"""
+	images = FilePath(__file__).parent().child('images')
+	assert images.isdir(), "%r is not a dir or does not exist" % (images,)
 
-	def test_png_size(self):
+	def test_pngSize(self):
 		for dim in [(2901, 1901), (900, 590), (7, 5)]:
-			im = FilePath(__file__).parent().child('images').child('%d_%d.png' % dim).open('rb')
-			self.assertEquals(dim, imgops.png_size(im))
+			im = self.images.child('%d_%d.png' % dim).open('rb')
+			self.assertEqual(dim, imgops.pngSize(im))
 			# Do it again.
-			self.assertEquals(dim, imgops.png_size(im))
+			self.assertEqual(dim, imgops.pngSize(im))
 
 
-	def test_gif_size(self):
+	def test_gifSize(self):
 		for dim in [(2901, 1901), (900, 590), (7, 5)]:
-			im = FilePath(__file__).parent().child('images').child('%d_%d.gif' % dim).open('rb')
-			self.assertEquals(dim, imgops.gif_size(im))
+			im = self.images.child('%d_%d.gif' % dim).open('rb')
+			self.assertEqual(dim, imgops.gifSize(im))
 			# Do it again.
-			self.assertEquals(dim, imgops.gif_size(im))
+			self.assertEqual(dim, imgops.gifSize(im))
 
 
-	def test_gif_size_notransparency_interlaced(self):
+	def test_gifSize_notransparency_interlaced(self):
 		for dim in [(2901, 1901), (900, 590), (7, 5)]:
-			im = FilePath(__file__).parent().child('images').child('%d_%d_notransparency_interlaced.gif' % dim).open('rb')
-			self.assertEquals(dim, imgops.gif_size(im))
+			im = self.images.child('%d_%d_notransparency_interlaced.gif' % dim).open('rb')
+			self.assertEqual(dim, imgops.gifSize(im))
 			# Do it again.
-			self.assertEquals(dim, imgops.gif_size(im))
+			self.assertEqual(dim, imgops.gifSize(im))
+
+
+
+class IsPngTests(unittest.TestCase):
+
+	def test_isPng(self):
+		self.assertTrue(imgops.isPng('\x89\x50\x4E\x47\x0D\x0A\x1A\x0A'))
+		self.assertTrue(imgops.isPng('\x89\x50\x4E\x47\x0D\x0A\x1A\x0AX'))
+		self.assertFalse(imgops.isPng('\x89\x50\x4E\x47\x0D\x0A\x1A\x0B'))
+
+
+	def test_isPngTypeError(self):
+		self.assertRaises(TypeError, lambda: imgops.isPng(None))
+		self.assertRaises(TypeError, lambda: imgops.isPng(1))
+		self.assertRaises(TypeError, lambda: imgops.isPng(u'x'))
+		self.assertRaises(TypeError, lambda: imgops.isPng(object()))
