62df9587d8715612fe9ba49a83387d0b47fcd30f
ivank <ivank@ludios.net>
2010-04-25 19:16:33 -0700

    test_constant.py: Improve tests; test Constant and BaseConstant more explicitly

--- mypy/test/test_constant.py
+++ mypy/test/test_constant.py
@@ -1,72 +1,99 @@
 from twisted.trial import unittest
 
-from mypy.constant import GenericIdentifier, InvalidIdentifier, Constant
+from mypy.constant import (
+	BaseConstant, Constant, GenericIdentifier, InvalidIdentifier)
 
 
-class _DummyId(GenericIdentifier):
+class AnyConstant(BaseConstant):
+	__slots__ = ()
+
+	@classmethod
+	def preCheck(cls, value):
+		pass
+
+
+
+class AnyConstant2(BaseConstant):
+	__slots__ = ()
+
+	@classmethod
+	def preCheck(cls, value):
+		pass
+
+
+
+class DummyId(GenericIdentifier):
 	_expectedLength = 16
 	__slots__ = ()
 
 
 
-class _DummyId2(GenericIdentifier):
-	_expectedLength = 16
-	__slots__ = ()
-
-
-
-class TestGenericIdentifier(unittest.TestCase):
+class BaseConstantTests(unittest.TestCase):
 	"""
-	Tests for L{constant.GenericIdentifier}
+	Tests for L{constant.BaseConstant}
 	"""
 	def test_equal(self):
-		s1 = _DummyId('z' * 16)
-		s2 = _DummyId('z' * 16)
+		s1 = AnyConstant('z' * 16)
+		s2 = AnyConstant('z' * 16)
 		self.assertEqual(s1, s2)
 
 
 	def test_notEqual(self):
-		s1 = _DummyId('z' * 16)
-		s2 = _DummyId('y' * 16)
+		s1 = AnyConstant('z' * 16)
+		s2 = AnyConstant('y' * 16)
 		self.assertNotEqual(s1, s2)
 
 
 	def test_notEqualBecauseDifferentTypes(self):
-		s1 = _DummyId('a' * 16)
+		s1 = AnyConstant('a' * 16)
 		self.assertNotEqual(s1, 0)
 		self.assertNotEqual(0, s1)
 		self.assertNotEqual(s1, 'z' * 16)
 
-		s2 = _DummyId2('a' * 16)
+		s2 = AnyConstant2('a' * 16)
 		self.assertNotEqual(s1, s2)
 
 
 	def test_repr(self):
-		s1 = _DummyId('z' * 16)
+		s1 = AnyConstant('z' * 16)
 		self.assertIn(repr('z' * 16), repr(s1))
 
 
 	def test_hash(self):
-		s1 = _DummyId('z' * 16)
-		s2 = _DummyId('z' * 16)
+		s1 = AnyConstant('z' * 16)
+		s2 = AnyConstant('z' * 16)
 		self.assertEqual(hash(s1), hash(s2))
 
 
+
+class ConstantTests(unittest.TestCase):
+	"""
+	Tests for L{constant.Constant}
+	"""
+	def test_wrongType(self):
+		self.assertRaises(TypeError, lambda: Constant(u'z' * 16))
+		self.assertRaises(TypeError, lambda: Constant(u'z' * 17))
+		self.assertRaises(TypeError, lambda: Constant(u''))
+		self.assertRaises(TypeError, lambda: Constant(3))
+		self.assertRaises(TypeError, lambda: Constant(3.0))
+		self.assertRaises(TypeError, lambda: Constant(300000000000000000000000))
+		self.assertRaises(TypeError, lambda: Constant([]))
+		self.assertRaises(TypeError, lambda: Constant({}))
+		self.assertRaises(TypeError, lambda: Constant(None))
+		self.assertRaises(TypeError, lambda: Constant(True))
+		self.assertRaises(TypeError, lambda: Constant(False))
+
+
+
+class GenericIdentifierTests(unittest.TestCase):
+	"""
+	Tests for L{constant.GenericIdentifier}
+	"""
 	def test_wrongLength(self):
-		self.assertRaises(InvalidIdentifier, lambda: _DummyId('z' * 15))
-		self.assertRaises(InvalidIdentifier, lambda: _DummyId('z' * 17))
-		self.assertRaises(InvalidIdentifier, lambda: _DummyId(''))
+		self.assertRaises(InvalidIdentifier, lambda: DummyId('z' * 15))
+		self.assertRaises(InvalidIdentifier, lambda: DummyId('z' * 17))
+		self.assertRaises(InvalidIdentifier, lambda: DummyId(''))
 
 
 	def test_wrongType(self):
-		self.assertRaises(TypeError, lambda: _DummyId(u'z' * 16))
-		self.assertRaises(TypeError, lambda: _DummyId(u'z' * 17))
-		self.assertRaises(TypeError, lambda: _DummyId(u''))
-		self.assertRaises(TypeError, lambda: _DummyId(3))
-		self.assertRaises(TypeError, lambda: _DummyId(3.0))
-		self.assertRaises(TypeError, lambda: _DummyId(300000000000000000000000))
-		self.assertRaises(TypeError, lambda: _DummyId([]))
-		self.assertRaises(TypeError, lambda: _DummyId({}))
-		self.assertRaises(TypeError, lambda: _DummyId(None))
-		self.assertRaises(TypeError, lambda: _DummyId(True))
-		self.assertRaises(TypeError, lambda: _DummyId(False))
+		self.assertRaises(TypeError, lambda: Constant(u'z' * 16))
