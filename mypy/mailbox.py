c302fda22ce200a2502f614e5b68ea3f38b17e23
ivank <ivank@ludios.net>
2010-05-06 06:16:53 +0000

    mailbox.py: add stoppedSpinningCb for Mailbox; add mailboxify decorator; write docs

--- mypy/mailbox.py
+++ mypy/mailbox.py
@@ -1,3 +1,4 @@
+import functools
 from collections import deque
 
 
@@ -5,15 +6,24 @@ class Mailbox(object):
 	"""
 	A method mailbox, similar to what you'll find in Erlang processes.
 
-	This may help you improve code where objects are making complicated
-	reentrant calls on each other.
+	This effectively flattens the call stacks when two objects are
+	reentrantly calling each other, and allows you to do something
+	when the mailbox is empty (for example, write buffered data to a socket).
+
+	This may be useful if you have an object A which both:
+		1) calls something, which in return methods on A
+		2) is called without calling anything first
 
 	See Minerva for sample use of this.
 	"""
-	__slots__ = ('object', '_spinning', '_pending')
+	__slots__ = ('_stoppedSpinningCb', '_spinning', '_pending')
 
-	def __init__(self, object):
-		self.object = object
+	def __init__(self, stoppedSpinningCb):
+		"""
+		L{stoppedSpinningCb} is a callable which will be called every time
+			Mailbox stops spinning.
+		"""
+		self._stoppedSpinningCb = stoppedSpinningCb
 		self._spinning = False
 		self._pending = deque()
 
@@ -21,16 +31,52 @@ class Mailbox(object):
 	def _spin(self):
 		assert not self._spinning, self._spinning
 		self._spinning = True
-		obj = self.object
 		try:
 			while self._pending:
-				method, args = self._pending.popleft()
-				getattr(obj, method)(*args)
+				callable, args, kwargs = self._pending.popleft()
+				callable(*args, **kwargs)
 		finally:
 			self._spinning = False
+			# TODO: maybe pass in the error, or a `hadError` boolean?
+			self._stoppedSpinningCb()
 
 
-	def _addMail(self, method, *args):
-		self._pending.append((method, args))
+	def addMail(self, callable, *args, **kwargs):
+		self._pending.append((callable, args, kwargs))
 		if not self._spinning:
 			self._spin()
+
+
+
+def mailboxify(mailboxAttr):
+	"""
+	`mailboxify` a method on a class. L{mailboxAttr} is the attribute
+	name for the mailbox to use.
+
+	Sample use:
+
+	class Something(object):
+		def __init__(self):
+			self._mailbox = Mailbox(self._stoppedSpinning)
+
+		def _stoppedSpinning(self):
+			# things to do after all mailbox messages are
+			# processed
+
+		@mailboxify('_mailbox')
+		def doThing(self):
+			# things
+
+		@mailboxify('_mailbox')
+		def anotherThing(self):
+			# things
+	"""
+	def wrapper(method):
+		@functools.wraps(method)
+		def newmethod(self, *args, **kwargs):
+			# method is an unbound function here, so prepend self
+			args = (self,) + args
+			getattr(self, mailboxAttr).addMail(method, *args, **kwargs)
+		return newmethod
+
+	return wrapper
