795cdd5d4aa2f0068cabc5f93561b52ad44d96e4
ivank <ivank@ludios.net>
2010-05-07 09:50:19 +0000

    mypy/test/test_mailbox.py: Add some real tests

--- mypy/test/test_mailbox.py
+++ mypy/test/test_mailbox.py
@@ -2,6 +2,7 @@ import traceback
 from twisted.trial import unittest
 
 from mypy import mailbox
+from mypy import iterops
 
 
 def getStackDepth():
@@ -9,6 +10,47 @@ def getStackDepth():
 	Get the current stack depth, excluding this function.
 	"""
 	return len(traceback.extract_stack()) - 1
+
+
+class MailboxTests(unittest.TestCase):
+	"""
+	Tests for L{mailbox.Mailbox}.
+	"""
+	def test_mailbox(self):
+		log = []
+
+		def stoppedSpinning(*args, **kwargs):
+			log.append(('stoppedSpinning', args, kwargs, getStackDepth()))
+
+		def a(*args, **kwargs):
+			log.append(('a', args, kwargs, getStackDepth()))
+			m.addMail(b, 3, 4, five=5, six=6)
+
+		def b(*args, **kwargs):
+			log.append(('b', args, kwargs, getStackDepth()))
+
+		m = mailbox.Mailbox(stoppedSpinning)
+		m.addMail(a)
+		# mailbox spins, a is called, b is called, stoppedSpinning is called
+
+		filteredLog = list(entry[0:3] for entry in log) # Don't include stack depths
+		self.assertEqual([
+			('a', (), {}),
+			('b', (3, 4), dict(five=5, six=6)),
+			('stoppedSpinning', (), {}),
+		], filteredLog)
+
+		stackDepths = list(entry[3] for entry in log)
+		if not iterops.areAllEqual(stackDepths):
+			self.fail("stack depths should have been all equal, were %r" % (stackDepths,))
+
+
+	def test_stoppedSpinningAlwaysCalled(self):
+		"""
+		The stoppedSpinning callback is called even if a callable raised
+		an exception.
+		"""
+
 
 
 # TODO
@@ -23,7 +65,3 @@ def getStackDepth():
 #
 #
 #
-#class MailboxTests(unittest.TestCase):
-#	def test_mailbox(self):
-#		demo = DemoClass()
-#		m = mailbox.Mailbox(demo)
