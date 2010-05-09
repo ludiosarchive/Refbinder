7280f3fcdc407af7e007265c611f4ba988a65bc3
ivank <ivank@ludios.net>
2010-05-09 03:51:09 +0000

    strops: add StringFragment and tests; assertEquals -> assertEqual

--- mypy/strops.py
+++ mypy/strops.py
@@ -21,5 +21,44 @@ def rreplace(string, needle, replacement):
 	return (string[:location] + replacement + string[location + len(needle):])
 
 
+
+class StringFragment(tuple):
+	"""
+	Represents a fragment of a string. Used to avoid copying, especially in
+	network protocols.
+	"""
+	__slots__ = ()
+
+	def __new__(cls, string, pos, size):
+		"""
+		C{string} is a C{str} object.
+		C{pos} is the position the fragment starts at.
+		C{size} is the size of the fragment.
+		"""
+		return tuple.__new__(cls, (string, pos, size))
+
+
+	def __repr__(self):
+		return '<%s for object at 0x%x, pos=%r, size=%r>' % (
+			self.__class__.__name__, id(self[0]), self[1], self[2])
+
+
+	def toBuffer(self):
+		"""
+		Return a C{buffer} object for the fragment. Note that Python
+		will not collect the underlying string object if there is a buffer
+		of it.
+		"""
+		return buffer(self[0], self[1], self[2])
+
+
+	# TODO: toMemoryview
+
+	def toString(self):
+		pos = self[1]
+		return self[0][pos:pos+self[2]]
+
+
+
 from pypycpyo import optimizer
 optimizer.bind_all_many(vars(), _postImportVars)
--- mypy/test/test_strops.py
+++ mypy/test/test_strops.py
@@ -4,7 +4,33 @@ from mypy import strops
 
 
 class RreplaceTests(unittest.TestCase):
+	"""
+	Tests for L{strops.rreplace}
+	"""
 	def test_rreplace(self):
-		self.assertEquals('.pych.pycello.py', strops.rreplace('.pych.pycello.pyc', '.pyc', '.py'))
-		self.assertEquals('34', strops.rreplace('33', '3', '4'))
-		self.assertEquals('hello', strops.rreplace('hello', 'z', 'x'))
+		self.assertEqual('.pych.pycello.py', strops.rreplace('.pych.pycello.pyc', '.pyc', '.py'))
+		self.assertEqual('34', strops.rreplace('33', '3', '4'))
+		self.assertEqual('hello', strops.rreplace('hello', 'z', 'x'))
+
+
+
+class StringFragmentTests(unittest.TestCase):
+	"""
+	Tests for L{strops.StringFragment}
+	"""
+	def test_stringFragmentFull(self):
+		f = strops.StringFragment("helloworld", 0, 10)
+		self.assertEqual("helloworld", f.toString())
+		self.assertEqual("helloworld", str(f.toBuffer()))
+
+
+	def test_stringFragmentPartial(self):
+		f = strops.StringFragment("helloworld", 1, 4)
+		self.assertEqual("ello", f.toString())
+		self.assertEqual("ello", str(f.toBuffer()))
+
+
+	def test_repr(self):
+		f = strops.StringFragment("helloworld", 1, 4)
+		self.assertTrue(repr(f).startswith("<StringFragment for object at 0x"), f)
+		self.assertTrue(repr(f).endswith(", pos=1, size=4>"), f)
