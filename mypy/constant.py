4c017c7d40bc39cb77af345c0de7a59b5229a32d
ivank <ivank@ludios.net>
2010-05-11 14:10:37 +0000

    constant.py: allow customizing the class attribute for the marker

--- mypy/constant.py
+++ mypy/constant.py
@@ -1,23 +1,43 @@
+import types
 import operator
 
 _postImportVars = vars().keys()
 
 
-class MarkerAttacher(type):
+class _MarkerAttacher(type):
+	def __new__(meta, class_name, bases, new_attrs):
+		cls = type.__new__(meta, class_name, bases, new_attrs)
+		setattr(cls, meta._attrName, object())
+		return cls
+
+
+
+def attachClassMarker(attrName):
 	"""
-	A metaclass that attaches a unique and hashable marker to the class.
+	A function that returns a metaclass that attaches a unique and
+	hashable marker to the class. C{attrName} is the name for the
+	marker attribute (on the class).
 
 	This is useful for immutable types that subclass tuple and
-	want a marker as the first item of the tuple to make different
+	want a marker as the first item of the tuple, to make different
 	types not equal to each other.
 
 	You may be surprised to learn that an object() hashes and
 	is immutable.
+
+	Sample usage:
+
+	class MyImmutable(tuple):
+		__slots__ = ()
+		__metaclass__ = MarkerAttacher('_MARKER')
+
+		def __new__(cls, value):
+			cls.preCheck(value)
+			return tuple.__new__(cls, (cls._MARKER, value))
 	"""
-	def __new__(meta, class_name, bases, new_attrs):
-		cls = type.__new__(meta, class_name, bases, new_attrs)
-		cls._MARKER = object()
-		return cls
+	metaclass = types.ClassType('_MarkerAttacher_' + attrName, (_MarkerAttacher,), {})
+	metaclass._attrName = attrName
+	return metaclass
 
 
 
@@ -29,7 +49,7 @@ class BaseConstant(tuple):
 	non-equal, but a marker as the first element is probably faster.
 	"""
 	__slots__ = ()
-	__metaclass__ = MarkerAttacher
+	__metaclass__ = attachClassMarker('_MARKER')
 
 	value = property(operator.itemgetter(1))
 
