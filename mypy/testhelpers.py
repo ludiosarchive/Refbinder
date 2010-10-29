8359591aa1e260b10611d64a506134e694fa4df7
ivank <ivank@ludios.net>
2010-10-29 20:43:37 +0000

    mypy.testhelpers: rename assertReallyUnequal -> assertReallyNotEqual

--- mypy/testhelpers.py
+++ mypy/testhelpers.py
@@ -6,7 +6,7 @@ class ReallyEqualMixin(object):
 		self.assertEqual(0, cmp(a, b))
 
 
-	def assertReallyUnequal(self, a, b):
+	def assertReallyNotEqual(self, a, b):
 		self.assertFalse(a == b)
 		self.assertTrue(a != b)
 		self.assertNotEqual(0, cmp(a, b))
