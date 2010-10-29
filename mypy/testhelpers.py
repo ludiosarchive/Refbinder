64217cf353f7f2f26eb22ff7611dd1b76175809f
ivank <ivank@ludios.net>
2010-10-29 20:21:35 +0000

    mypy/testhelpers.py: add @todo decorator for decorating Twisted trial tests

--- mypy/testhelpers.py
+++ mypy/testhelpers.py
@@ -10,3 +10,34 @@ class ReallyEqualMixin(object):
 		self.assertFalse(a == b)
 		self.assertTrue(a != b)
 		self.assertNotEqual(0, cmp(a, b))
+
+
+
+def todo(reasonOrMethod):
+	"""
+	This is a both a decorator and a decorator-returner, depending on
+	what C{reasonOrMethod} is.
+
+	This allows you to do
+
+	@todo
+	def test_that_will_fail(self):
+		...
+
+	or
+
+	@todo("some reason or explanation")
+	def test_that_will_fail2(self):
+		...
+	"""
+
+	if hasattr(reasonOrMethod, '__call__'):
+		method = reasonOrMethod
+		method.todo = 'todo'
+		return method
+
+	def decorator(method):
+		method.todo = reasonOrMethod
+		return method
+
+	return decorator
