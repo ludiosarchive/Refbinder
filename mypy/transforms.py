d25bbad602c560d5130c9e205f9c7b4e99eb52be
ivank <ivank@ludios.net>
2010-11-18 04:36:53 +0000

    Add mypy/transforms.py

---
+++ mypy/transforms.py
@@ -0,0 +1,16 @@
+"""
+Some useful transforms for L{filecache.FileCache}.  It's useful to have them
+in one place to prevent duplicated entries in FileCache's content cache.
+"""
+
+from hashlib import md5
+
+_postImportVars = vars().keys()
+
+
+def md5hexdigest(s):
+	return md5(s).hexdigest()
+
+
+from pypycpyo import optimizer
+optimizer.bind_all_many(vars(), _postImportVars)
