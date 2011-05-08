722d4279e8ba8a1da881fe1c9633a6262f332ad6
Ivan Kozik <ivan@ludios.org>
2011-05-08 17:15:25 +0000

    Update setup.py for release to PyPI; add MANIFEST.in; update .gitignore

--- setup.py
+++ setup.py
@@ -7,7 +7,19 @@ import mypy
 setup(
 	name='Mypy',
 	version=mypy.__version__,
-	description="Python utilities usable in a wide variety of applications.",
+	description=("Python utilities: constant binder for performance, "
+		"file cache, faster urandom, StringFragment, Constant, "
+		"ReallyEqualMixin, and more."),
+	url="https://github.com/ludios/Mypy",
+	author="Ivan Kozik",
+	author_email="ivan@ludios.org",
+	classifiers=[
+		'Programming Language :: Python :: 2',
+		'Development Status :: 3 - Alpha',
+		'Operating System :: OS Independent',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+	],
 	packages=['mypy', 'mypy.test'],
 	package_data={'mypy.test': ['images/*']},
 )
